25190d4b6be9fece418c3a584e47e9942374f7bd
James Ratcliffe <james.ratcliffe@lightspeedhq.com>
2019-01-07 16:39:12 -0500

    Write Open in Gitup command

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const cp = require('child_process');
+const path = require('path');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,11 +19,16 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('extension.openInGitup', function () {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		const fileDir = path.dirname(vscode.window.activeTextEditor.document.fileName);
+		const script = `cd ${fileDir} && gitup`;
+		cp.exec(script, function(error, stdout, stderr) {
+			if (error) {
+				console.error(`exec error: ${error}`);
+				return;
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
